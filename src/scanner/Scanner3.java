364b39f208db0a6ac5b6a4b811b4d30063835b8a
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-04-14 23:32:48 +0900

    feat : studied Scanner example

---
+++ src/scanner/Scanner3.java
@@ -0,0 +1,13 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Scanner2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
